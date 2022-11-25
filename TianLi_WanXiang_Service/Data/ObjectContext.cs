1d76890c6e644e8ce0e00beb6ef397a58ac431af
GengGode <GengGode@outlook.com>
2022-11-25 23:26:56 +0800

    feat: 添加了Sql数据库和对应的数据类型

---
+++ TianLi_WanXiang_Service/Data/ObjectContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TianLi_WanXiang_Service.Models;
+
+namespace TianLi_WanXiang_Service.Data
+{
+    public class ObjectContext :DbContext
+    {
+        public ObjectContext(DbContextOptions<ObjectContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Object> Objects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Object>().ToTable("Object");
+        }
+    }
+}
---
+++ TianLi_WanXiang_Service/Models/Object.cs
@@ -0,0 +1,14 @@
+﻿namespace TianLi_WanXiang_Service.Models
+{
+    public class Object
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public int MapId { get; set; }
+        public string ObjectName { get; set; } = string.Empty;
+        public string ExtendedFieldJson { get; set; } = string.Empty;
+    }
+}
