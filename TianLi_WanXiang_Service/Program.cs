f05747fba0d9fa301b0ff6badd3c9cd8408ff6ac
GengGode <GengGode@outlook.com>
2022-11-25 23:40:43 +0800

    feat: 添加创建数据库

--- TianLi_WanXiang_Service/Program.cs
+++ TianLi_WanXiang_Service/Program.cs
@@ -1,35 +1,45 @@
-namespace TianLi_WanXiang_Service
+using TianLi_WanXiang_Service.Data;
+using TianLi_WanXiang_Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add DbContext
+builder.Services.AddDbContext<ObjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WanXiangContext")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ObjectContext>();
+    context.Database.EnsureCreated();
+}
+
+app.UseHttpsRedirection();
+
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
